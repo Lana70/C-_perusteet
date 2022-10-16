1cf906cd97a734dba4672b594afa6b8b9736016a
Svetlana <115741553+Lana70@users.noreply.github.com>
2022-10-16 19:18:44 +0300

    harjoitus
    Ikä tehtävä

---
+++ Harjoitus_ikä_tehtävä/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ikä_tehtävä
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string num = textBox1.Text;
+            if (num != "")
+            {
+                textBox2.Text = $"Olet {num} vuotta";
+
+            }
+            else
+            {
+                textBox2.Text = "";
+            }
+        }
+
+
+    }
+}
