68be2e9a12813bb9fd159454f4e9ce7cfa19484f
Svetlana <115741553+Lana70@users.noreply.github.com>
2022-10-16 19:34:27 +0300

    Harjoitus
    Keskiarvo laskuri

---
+++ Harjoitus_keskiarvo/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace keskiarvo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c, result;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            c = Convert.ToDouble(textBox3.Text);
+            result = (a + b + c) / 3;
+            textBox4.Text = Convert.ToString(result);
+        }
+    }
+}
